3d568d13316f89c05e36c9f4dfc188aef6f0c95c
Gonzalo Cañada Benlloch <gonzalocbenlloch@gmail.com>
2019-08-06 19:59:17 +0200

    Added default date

--- src/component/month-picker.js
+++ src/component/month-picker.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './../css/month-picker.css';
 import '../css/picker-styles.css';
 
@@ -8,10 +9,15 @@ export default class MonthPicker extends Component {
     constructor(props){
         let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
         super(props);
-        if(this.props.months && this.props.months.length === 12)
-            months = this.props.months;
 
-        this.state = {cells:[], selectedDate: new Date(0), currentView:"years",renderDate:false, months:months};
+        this.state = {
+            cells: [],
+            selectedDate: props.selectedDate || moment(),
+            currentView: props.selectedDate ? "months" : "years",
+            renderDate: props.selectedDate ? true : false,
+            months: props.months || months
+        };
+
         this.selectCell = this.selectCell.bind(this);
         this.previous = this.previous.bind(this);
         this.next = this.next.bind(this);
@@ -19,16 +25,20 @@ export default class MonthPicker extends Component {
 
     componentWillMount(){
         let cells =[];
-        let year = new Date().getFullYear() - 6 ;
-        for(let i = 0 ; i< 12 ; i++)
-            cells.push(year+i);
+        if (this.state.currentView === "years") {
+            let year = moment().year() - 6 ;
+            for (let i = 0 ; i< 12 ; i++)
+                cells.push(year+i);
+        } else {
+            cells = this.state.months;
+        }
         this.setState({cells:cells});
     }
 
     selectCell(cellContent, index){
         if(typeof cellContent === 'number'){
             let date = this.state.selectedDate;
-            date.setFullYear(cellContent);
+            date.year(cellContent);
             this.setState({selectedDate:date});
 
             let months = this.state.months;
@@ -36,7 +46,7 @@ export default class MonthPicker extends Component {
             this.setState({cells:months});
         }else{
             let date = this.state.selectedDate;
-            date.setMonth(index);
+            date.month(index);
             this.setState({selectedDate:date, renderDate:true});
             if(this.props.onChange && typeof this.props.onChange === "function")
                 this.props.onChange(this.state.selectedDate);
@@ -51,7 +61,7 @@ export default class MonthPicker extends Component {
             this.setState({cells:years, renderDate:false});
         }else{
             let cells =[];
-            let year = new Date().getFullYear() - 6 ;
+            let year = moment().year() - 6 ;
             for(let i = 0 ; i< 12 ; i++)
                 cells.push(year+i);
             this.setState({cells:cells});
@@ -67,7 +77,7 @@ export default class MonthPicker extends Component {
             this.setState({cells:years, renderDate:false});
         }else{
             let cells =[];
-            let year = new Date().getFullYear() - 6 ;
+            let year = moment().year() - 6 ;
             for(let i = 0 ; i< 12 ; i++)
                 cells.push(year+i);
             this.setState({cells:cells});
@@ -78,10 +88,7 @@ export default class MonthPicker extends Component {
     render() {
         let selectedString="";
         if(this.state.renderDate){
-            let selectedMonth = this.state.selectedDate.getMonth()+1;
-            let selectedYear =  this.state.selectedDate.getFullYear();
-            let monthString = (selectedMonth)>9 ? selectedMonth : "0"+selectedMonth;
-            selectedString = monthString+"-"+selectedYear;
+            selectedString = this.state.selectedDate.format("M-YYYY");
         }
         let head =
             <div className="section_mp group_mp">
